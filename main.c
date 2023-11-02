eae7bec1469a1cf2e99523900ef3f6c6c51c03a7
Ukeme Edet <ukemeedet2207@gmail.com>
2023-11-02 22:27:41 +0100

    Betty would be proud

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - The main function for the simple shell project
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("Welcome to the simple shell project!\n");
+	return (0);
+}
